f3c5bb0a651d8397a6fb7952d7d1e77977d10e0e
Mathias Brökelmann <mbroekelmann@gmail.com>
2011-03-05 00:40:26 +0100

    review color definitions

--- src/main/scala/imagemagick/Color.scala
+++ src/main/scala/imagemagick/Color.scala
@@ -8,33 +8,36 @@ import scala.math.{min, max}
  * Time: 22:12
  */
 
-object Color {
-  def color(s: String): Color = new Color {
-    val spec = if (s.startsWith("#")) format("'%s'", s) else s
-  }
-
+trait ColorSpecification {
   /**
    * predefined colors
    */
-  lazy val black = color("#000000")
-  lazy val white = color("#FFFFFF")
-  lazy val red = color("#FF0000")
-  lazy val green = color("#00FF00")
-  lazy val blue = color("#0000FF")
-  lazy val none = color("#00000000")
+  lazy val black = Color("#000000")
+  lazy val white = Color("#FFFFFF")
+  lazy val red = Color("#FF0000")
+  lazy val green = Color("#00FF00")
+  lazy val blue = Color("#0000FF")
+  lazy val none = Color("#00000000")
   lazy val opaque = black
   lazy val transparent = none
+
+  def color(name: String) = Color(name)
+  def color(red: Int, green: Int, blue: Int) = Color(red, green, blue)
+}
+
+object Color {
+
+  def apply(s: String): Color = new Color {
+    lazy val spec = if (s.startsWith("#")) format("%s", s) else s
+  }
 
   implicit def boxedValue(value: Int) = new {
     def boxed(lower: Int, upper: Int): Int = max(lower, min(upper, value))
   }
 
-  def rgb(red: Int, green: Int, blue: Int) = new Color {
-
-    val spec = format("rgb(%d,%d,%d)", red boxed (0, 255), green boxed (0, 255), blue boxed (0, 255))
+  def apply(red: Int, green: Int, blue: Int) = new Color {
+    lazy val spec = format("rgb(%d,%d,%d)", red boxed (0, 255), green boxed (0, 255), blue boxed (0, 255))
   }
 }
 
-trait Color {
-  def spec: String
-}
+trait Color extends Parameter
--- src/test/scala/imagemagick/ColorSpec.scala
+++ src/test/scala/imagemagick/ColorSpec.scala
@@ -13,24 +13,24 @@ class ColorSpec extends Specification {
 
   "Color" should {
     "define named colors" in {
-      color("black").spec must_== "black"
+      Color("black").spec must_== "black"
     }
 
     "define rgb colors using hex" in {
-      color("#ff0000").spec must_== "'#ff0000'"
+      Color("#ff0000").spec must_== "#ff0000"
     }
 
     "define rgb colors" in {
-      rgb(0, 127, 255).spec must_== "rgb(0,127,255)"
+      Color(0, 127, 255).spec must_== "rgb(0,127,255)"
     }
 
     "define rgb colors with 0 to 255 ranges" in {
-      rgb(-100, 12312313, 0).spec must_== "rgb(0,255,0)"
+      Color(-100, 12312313, 0).spec must_== "rgb(0,255,0)"
     }
 
 
     "define rgb colors with " in {
-      rgb(0, 127, 255).spec must_== "rgb(0,127,255)"
+      Color(0, 127, 255).spec must_== "rgb(0,127,255)"
     }
   }
 }
