4c2d5735aa039a7d839b1c61c89bcb9643d70d00
Mathias Brökelmann <mbroekelmann@gmail.com>
2011-03-06 23:13:42 +0100

    use flagable in image geometry

--- src/main/scala/imagemagick/ImageGeometry.scala
+++ src/main/scala/imagemagick/ImageGeometry.scala
@@ -9,25 +9,45 @@ package org.imagemagick
  * Date: 04.03.11 21:48
  * Time: 21:48
  */
-trait ImageGeometry extends Parameter {
+trait ImageGeometry extends Parameter
+
+/**
+ * identifies an image geometry with specified size
+ */
+trait SizeImageGeometry extends ImageGeometry {
 
   /**
-   * optionally define an offset
+   * define an offset for the size
    */
-  def offset(x: Int, y: Int) = new ImageGeometry {
-    def spec =
-      ImageGeometry.this.spec +
-        (if (x >= 0) "+" + x else x.toString) +
-        (if (y >= 0) "+" + y else y.toString)
-    
-    override def escape = ImageGeometry.this.escape
-  }
+  def offset(x: Int, y: Int) = OffsetImageGeometry(this, x, y)
 
+  /**
+   * alternative to define an offset
+   */
   def apply(x: Int, y: Int) = offset(x, y)
 }
 
+/**
+ * identifies a size image geometry of width and height which was flaged
+ */
+case class FlagedImageGeometry(spec: String, override val escape: Boolean) extends SizeImageGeometry
+
+/**
+ * identifies an image geometry with an offset
+ */
+case class OffsetImageGeometry(size: SizeImageGeometry, x: Int, y: Int) extends ImageGeometry {
+  def spec = size.spec +
+    (if (x >= 0) "+" + x else x.toString) +
+    (if (y >= 0) "+" + y else y.toString)
+
+  override def escape = size.escape
+}
+
+/**
+ * mixin trait for image geometry definitions
+ */
 trait ImageGeometryDefinition {
-  def apply(pixel: Int) = new ImageGeometry {
+   def apply(pixel: Int) = new SizeImageGeometry {
 
     /**
      * treat pixel as width
@@ -42,22 +62,21 @@ trait ImageGeometryDefinition {
     /**
      * Resize image to have specified area in pixels. Aspect ratio is preserved.
      */
-    def area = new ImageGeometry {
+    def area = new SizeImageGeometry {
       def spec = pixel + "@"
+
       override def escape = true
     }
   }
 
-  def apply(width: Option[Int] = None, height: Option[Int] = None) = {
+  def apply(width: Option[Int] = None, height: Option[Int] = None): SizeImageGeometry = {
     (width, height) match {
       case (None, None) => error("either width or height must be defined at least")
-      case (Some(w), Some(h)) => new ImageGeometry {
-        def spec = w + "x" + h
-      }
-      case (Some(w), _) => new ImageGeometry {
+      case (Some(w), Some(h)) => apply(w, h)
+      case (Some(w), _) => new SizeImageGeometry {
         def spec = w.toString
       }
-      case (_, Some(h)) => new ImageGeometry {
+      case (_, Some(h)) => new SizeImageGeometry {
         def spec = "x" + h
       }
     }
@@ -66,91 +85,31 @@ trait ImageGeometryDefinition {
   /**
    * Maximum values of height and width given, aspect ratio preserved.
    */
-  def apply(width: Int, height: Int) = new ImageGeometry {
+  def apply(width: Int, height: Int) = new SizeImageGeometry with Flagable[SizeImageGeometry] {
     def spec = width + "x" + height
-
-    /**
-     * Minimum values of width and height given, aspect ratio preserved.
-     */
-    def min = new ImageGeometry {
-      def spec = width + "x" + height + "^"
-      override def escape = true
-    }
-
-    /**
-     * Minimum values of width and height given, aspect ratio preserved.
-     */
-    def ^ = min
-
-    /**
-     * Width and height emphatically given, original aspect ratio ignored.
-     */
-    def force = new ImageGeometry {
-      def spec = width + "x" + height + "!"
-      override def escape = true
-    }
-
-    /**
-     * Width and height emphatically given, original aspect ratio ignored.
-     */
-    def ! = force
-
-    /**
-     * Change as per widthxheight but only if an image dimension exceeds a specified dimension.
-     */
-    def ifGreater = new ImageGeometry {
-      def spec = width + "x" + height + ">"
-      override def escape = true
-    }
-
-    /**
-     * Change as per widthxheight but only if an image dimension exceeds a specified dimension.
-     */
-    def > = ifGreater
-
-    /**
-     * Change dimensions only if both image dimensions exceed specified dimensions.
-     */
-    def ifBothGreater = new ImageGeometry {
-      def spec = width + "x" + height + "<"
-      override def escape = true
-    }
-
-    /**
-     * Change dimensions only if both image dimensions exceed specified dimensions.
-     */
-    def < = ifBothGreater
+    def build(flag: String, esc: Boolean) =
+      FlagedImageGeometry(spec + flag, escape || esc)
   }
 
-  def apply(scale: Double) = new ImageGeometry {
+  def apply(scale: Double) = new SizeImageGeometry {
     assert(scale >= 0, "scale must be higher than 0")
 
     def spec = scale + "%"
+
     override def escape = true
   }
 
-  def apply(scalex: Double, scaley: Double) = new ImageGeometry {
+  def apply(scalex: Double, scaley: Double) = new SizeImageGeometry {
     assert(scalex >= 0, "scalex must be higher than 0")
     assert(scaley >= 0, "scaley must be higher than 0")
 
     def spec = scalex + "x" + scaley + "%"
+
     override def escape = true
   }
 }
 
+/**
+ * singleton for geometry definitions
+ */
 object Geometry extends ImageGeometryDefinition
-
-trait Parameter {
-  def spec: String
-  def escape: Boolean = false
-}
-
-case class ParameterOperation(operation: String, param: Parameter) extends Operation {
-  def commands = operation :: param.spec :: Nil
-}
-
-object ParameterOperation {
-  def apply(operation: String, value: Any) = new Operation {
-    def commands = operation :: value.toString :: Nil
-  }
-}
