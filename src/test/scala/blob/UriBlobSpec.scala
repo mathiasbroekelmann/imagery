ce18f55e5f856982135d97661758065b64e0bccf
Mathias Brökelmann <mbroekelmann@gmail.com>
2011-03-20 23:26:14 +0100

    uncomment to make test work independend from environment

--- src/test/scala/blob/UriBlobSpec.scala
+++ src/test/scala/blob/UriBlobSpec.scala
@@ -10,9 +10,10 @@ import java.io.File
  */
 
 class UriBlobSpec extends Specification {
+  /*
   val imageFile = new File("/media/fotos/2010/DSC05535.JPG").toURI
   "content type of " + imageFile + " should be image/jpeg"  in {
     val con = imageFile.toURL.openConnection
     con.getContentType must beEqual("image/jpeg")
-  }
+  }*/
 }
