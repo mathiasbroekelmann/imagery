c41350d312d2c54996f2f85de0ae05541c76ab18
Mathias Brökelmann <mbroekelmann@gmail.com>
2011-03-21 23:51:28 +0100

    empty class definition

---
+++ src/main/scala/resources/CropImage.scala
@@ -0,0 +1,14 @@
+package org.mbr.imagery.resources
+
+import javax.ws.rs.Path
+import org.mbr.imagery.page.PageContent
+
+/**
+ * User: mathias
+ * Date: 21.03.11 23:36
+ * Time: 23:36
+ */
+@Path("/crop")
+class CropImage extends PageContent {
+  
+}
