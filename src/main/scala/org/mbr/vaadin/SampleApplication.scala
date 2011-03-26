de2a89ef407bc25b163037a36638c8e14509f0c1
Mathias Brökelmann <mbroekelmann@gmail.com>
2011-03-27 00:12:50 +0100

    playing around with events and fragent for permanent urls

--- src/main/scala/org/mbr/vaadin/SampleApplication.scala
+++ src/main/scala/org/mbr/vaadin/SampleApplication.scala
@@ -8,10 +8,24 @@ import java.awt.event.ComponentListener
 import com.vaadin.ui.Component.{Listener, Event}
 import com.vaadin.ui.Window.CloseListener
 import com.vaadin.ui._
-import com.vaadin.terminal.Sizeable
-import java.io.File
 import java.net.URI
-import org.mbr.imagery.image.{StoredFileImage, StoredImage, ImageStore}
+import com.vaadin.data.Container.Hierarchical
+import scala.collection.JavaConversions._
+import reflect.{BeanProperty, BeanInfo}
+import com.vaadin.data.Container
+import com.vaadin.data.util.{FilesystemContainer, BeanItem, HierarchicalContainer, BeanItemContainer}
+import java.lang.{String, Class}
+import org.mbr.imagery.image.{Image, StoredFileImage, StoredImage, ImageStore}
+import org.mbr.imagery.blob.FileBlob
+import Filesystem._
+import org.apache.commons.io.IOUtils
+import org.apache.commons.io.output.ByteArrayOutputStream
+import com.vaadin.terminal.{Resource, StreamResource, Sizeable}
+import image.ResourceCache
+import java.io._
+import org.imagemagick.Convert._
+import org.imagemagick.{Convert, Color, Geometry}
+import com.vaadin.terminal.StreamResource.StreamSource
 
 /**
  * @author mathias.broekelmann
@@ -19,45 +33,144 @@ import org.mbr.imagery.image.{StoredFileImage, StoredImage, ImageStore}
  */
 class SampleApplication extends VaadinApplication {
 
+  application =>
+
   import Vaadin._
+
+  val mainWindow = new Window("Hello Matze!")
+  val mainPanel = new HorizontalSplitPanel
+  val imagesPanel = new CssLayout
+  val urifu = new UriFragmentUtility
 
   def init {
     buildMainLayout
   }
 
   def buildMainLayout = {
-    val mainWindow = new Window("Hello Matze!")
     setMainWindow(mainWindow)
-
-    val mainPanel = new HorizontalSplitPanel
-    mainPanel.setSizeFull
+    setTheme("imagery")
+    val layout = new CssLayout
+    mainWindow.setContent(layout)
+    layout.setSizeFull
+    layout.setMargin(false)
     mainPanel.setFirstComponent(createAlbumTree)
+    mainPanel.setSecondComponent(imagesPanel)
+
     mainPanel.setSplitPosition(200, Sizeable.UNITS_PIXELS)
-    mainWindow.setContent(mainPanel)
+    mainWindow.addComponent(mainPanel)
+    mainWindow.addComponent(urifu)
+    mainPanel.setSizeFull
+    imagesPanel.setSizeFull
+    urifu.setSizeUndefined
   }
 
   def createAlbumTree = {
-    val tree = new Tree
-    val store = new AlbumStore
-    for (child <- store.childStores) {
-      tree.addItem(child)
+    val filter = new FilenameFilter {
+      def accept(dir: File, name: String) = new File(dir, name).isDirectory && !name.startsWith(".")
+    }
+    val albumRoot = new File("/media/fotos")
+    val store = new FilesystemContainer(albumRoot, filter, true)
+    val tree = new Tree("Alben", store)
+    tree.addStyleName("album-nav")
+    tree.setItemCaptionPropertyId(FilesystemContainer.PROPERTY_NAME)
+    tree.setItemCaptionMode(AbstractSelect.ITEM_CAPTION_MODE_PROPERTY)
+    tree.setImmediate(true)
+    println("initial fragment path: " + urifu.getFragment)
+
+    /**
+     * expand album tree up to the given directory
+     */
+    def expand(album: File) {
+      if(album != albumRoot) {
+        expand(album.getParentFile)
+        tree.expandItem(album)
+      }
+    }
+
+    urifu.fragment {
+      case path => {
+        val album = new File(albumRoot, path)
+        if(album.exists && album.isDirectory && album.canRead) {
+          tree.setValue(album)
+          expand(album.getParentFile)
+        }
+      }
+    }
+    tree.valuechange {
+      case file: File => {
+        println("some album: " + file)
+        urifu.setFragment(file.getAbsolutePath.stripPrefix(albumRoot.getAbsolutePath))
+        showImagesIn(file)
+      }
     }
     tree
   }
+
+  def showImagesIn(directory: File) = {
+    val images = directory.collect {
+      case file: File if file.toURI.toURL.openConnection.getContentType.startsWith("image/") => createImage(file)
+    }
+    imagesPanel.removeAllComponents
+    for(image <- images.take(20)) {
+      val teaser: Embedded = new Embedded(null, image.thumbnail)
+      teaser.addStyleName("image-teaser")
+      imagesPanel.addComponent(teaser)
+      teaser.click {
+        case Left(_) => println("image: " + image.name + " left clicked")
+        case Right(_) => println("image: " + image.name + " right clicked")
+        case Middle(_) => println("image: " + image.name + " middle clicked")
+      }
+    }
+  }
+
+  def createImage(imageFile: File) = new Image with FileBlob with Name {
+
+    self =>
+
+    def file = imageFile
+
+    def name = imageFile.getName
+
+    def thumbnail: Resource = {
+
+      def create(in: InputStream, out: OutputStream): Unit = {
+        println("render thumbnail")
+        val width, height = 150
+        val size = Geometry(width, height)
+        convert.define(jpegSize(Geometry(width * 4, height * 4)))
+          .apply(image(in).buffered)
+          .autoOrient
+          .thumbnail(Geometry(width * 2 * height * 2).area)
+          .borderColor(Color("snow"))
+          .border(Geometry(5, 5))
+          .background(Color("black"))
+          .polaroid(0)
+          .resize(Geometry(50.0))
+          .writeAs("png").to(out)
+      }
+
+      val source = new StreamSource {
+        def getStream = {
+          println("get stream for " + name)
+          val out = new ByteArrayOutputStream
+          ResourceCache.cached(self, "thumbnail") {
+            cachedOut => self.read(create(_, cachedOut))
+          } write out
+          new ByteArrayInputStream(out.toByteArray)
+        }
+      }
+
+      new StreamResource(source, file.getName + "-thumbnail.png", application)
+    }
+
+    def getStream = read { input =>
+      val out = new ByteArrayOutputStream
+      IOUtils.copy(input, out)
+      new ByteArrayInputStream(out.toByteArray)
+    }.getOrElse(new ByteArrayInputStream(new Array[Byte](0)))
+  }
 }
 
-class AlbumStore(val parent: Option[AlbumStore] = None, val location: File = new File("/home/APERTO/mathias.broekelmann/Bilder/Fotos")) extends ImageStore {
-
-  val path = "/"
-
-  def storeUri = URI.create("/")
-
-  protected def storedFileImage(file: File, storeUri: URI) = StoredFileImage(file, storeUri)
-
-  protected def nestedImageStore(name: String) = new AlbumStore(Some(this), new File(location, name))
-
-  type Image = StoredImage
-  type Store = AlbumStore
-
-  override def toString = name
+trait Name {
+  def name: String
 }
